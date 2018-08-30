620b7d81070f5f5e928631c57747549e990cb278
Sixto Herrera García <herreras@unican.es>
2018-08-30 13:23:57 +0200

    Including the help

--- getCyclonTrack.R
+++ getCyclonTrack.R
@@ -1,7 +1,126 @@
-#######################################################################
-##                    CYCLONE TRACKING DETECTION                     ##
-#######################################################################
-## based on: http://www.europeanwindstorms.org/method/track/
+#     getCyclonTrack.R Algorithm to obtain the cyclon tracks from the cyclon centers
+#
+#     Copyright (C) 2018 Santander Meteorology Group (http://www.meteo.unican.es)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#' @title Cyclon Track Detection
+#' @description Implementation of an algorithm to obtain the cyclon track from the cyclon centers
+#'
+#' @param cyclonCenter List of cyclon centers detected for each day.
+#' @param seek.radius Maximum distance in degrees to look for a new cyclon center to be included in the same trajectory. The default value is 10.
+#' @param ndr.threshold Minimum value of the Normalized Deepeningun Ratio to be considered a cyclon center as explosive. The default value is 1.
+#' @param vo.threshold Minimum value of vorticity to be considered a candidate to cyclon center. The default value is 1e-4.
+#' @param cut Maximum length, in time steps, to be considered for the track. The default value is 4.
+#' @param cyclon.date Argument to obtain all the tracks occurred in an specific date. The default value is \code{NULL}.
+#' @param list.date Argument containing the dates corresponding to the cyclon centers. The default value is \code{NULL}.
+#' @param criteria Criteria considered to select the cyclon center between the possible candidates. Options implemented are:
+#' \code{"global"} (default): maximum of the Euclidean norm of a vector containing the vorticity, the NDR and the slp normalized;
+#' \code{"max.vo"}: point with the maximum vorticity; \code{"max.ndr"}: point with the maximum NDR; \code{"min.slp"}: point with the minimum slp.
+#'     
+#' @seealso \code{\link{getCyclonCenters}} for the algorithm obtaining the cyclon centers. 
+#' @return A list with the cyclon tracks detected. Each element contains the dates of the track and the matrix with the corresponding
+#' NDR, slp, vorticity, laplacian of the slp, longitude, latitude, windspeed and explosive character of the cyclon center.
+#' @family cyclonTrack
+#'
+#' @references
+#'
+#' \itemize{
+#' \item I. Iparragirre (2018) Climate change projections of explosive cyclogenesis events frequency in the Iberian Peninsula. Master's Thesis: Physics, Instrumentation and the Environment - Universidad de Cantabria, http://meteo.unican.es/en/theses/2018_Itsasne
+#' 
+#' }
+#' @author I. Iparragirre, M. D. Frías, S. Herrera
+#' @export
+#' @examples {
+#' ## We define the geographical and temporal domain to be loaded.
+#' lonLim <- c(-50,40)
+#' latLim <- c(15,75)
+#' r <- 6378.1 # Parameter: Earth radius
+#' season <- 1:12
+#' years <- c(2009:2010)
+#' 
+#' ## We consider the ERA-Interim dataset from the Santander User Data Gateway
+#' dataset <- "http://meteo.unican.es/tds5/dodsC/interim/daily/interim20_daily.ncml"
+#' di <- dataInventory(dataset = dataset)
+#' names(di)
+#' 
+#' ## Sea Level Pressure
+#' slp <- loadGridData(dataset = dataset, var = "SLP", season = season, years = years,
+#'                     lonLim = lonLim, latLim = latLim, time = "none", aggr.d = "none")
+#' ## The input of the algorithm should be expressed in hPa instead Pa:
+#' ao <- c(0,0.01)
+#' slp$Data <- ao[2]*(ao[1]+slp$Data)
+#' 
+#' ## Vorticity at 850 hPa
+#' zg <- loadGridData(dataset = dataset, var = "Z850", season = season, years = years,
+#'                    lonLim = lonLim, latLim = latLim, time = "none", aggr.d = "none")
+#' vo <- laplacian(zg)
+#' rm("zg")
+#' 
+#' ## Detection and Tracking parameters
+#' seek.radius <- 6
+#' slp.diff.threshold <- 10
+#' vo.diff.threshold <- 1e-6
+#' lap.diff.threshold <- 20
+#' ndr.threshold <- 1
+#' vo.threshold <- 1e-5
+#' criteria <- "global"
+#' 
+#' ## Detection and Tracking
+#' varOutputCenters <- getCyclonCenters(slp,vo, seek.radius = seek.radius, slp.diff.threshold = slp.diff.threshold,
+#'                                      vo.diff.threshold = vo.diff.threshold, lap.diff.threshold = lap.diff.threshold,
+#'                                      ndr.threshold = ndr.threshold, vo.threshold = vo.threshold, criteria = criteria)
+#' list.date<-slp$Dates$start
+#' cyclonTrack <- getCyclonTrack(varOutputCenters,
+#'                               seek.radius = seek.radius,
+#'                               ndr.threshold = ndr.threshold,
+#'                               vo.threshold = vo.threshold,
+#'                               max.length = 20,
+#'                               cyclon.date = NULL,
+#'                               list.date = list.date,
+#'                               criteria = criteria, cut = 20)
+#' 
+#' ## Plotting the results
+#' data("wrld")
+#' po <- SpatialPoints(cbind(cyclonTrack[[1]][[2]][,5], cyclonTrack[[1]][[2]][,6]))
+#' dat <- as.data.frame(cyclonTrack[[1]][[2]][,2])
+#' colnames(dat) <- "y"
+#' kl <- SpatialPointsDataFrame(po, data = dat)
+#' spplot(kl, zcol = "y", sp.layout = list(wrld, first = F), colorkey = TRUE, 
+#'               xlim = lonLim, ylim = latLim, xlab = slp$Dates$start[1])
+#'
+#' ## Considering an specific date
+#' cyclonTrack <- getCyclonTrack(varOutputCenters,
+#'                               seek.radius = seek.radius,
+#'                               ndr.threshold = ndr.threshold,
+#'                               vo.threshold = vo.threshold,
+#'                               max.length = 20,
+#'                               cyclon.date = "2010-02-26 12:00:00 GMT",
+#'                               list.date = list.date,
+#'                               criteria = criteria, cut = 20)
+#' 
+#' ## Plotting the results
+#' data("wrld")
+#' po <- SpatialPoints(cbind(cyclonTrack[[1]][[2]][,5], cyclonTrack[[1]][[2]][,6]))
+#' dat <- as.data.frame(cyclonTrack[[1]][[2]][,2])
+#' colnames(dat) <- "y"
+#' kl <- SpatialPointsDataFrame(po, data = dat)
+#' spplot(kl, zcol = "y", sp.layout = list(wrld, first = F), colorkey = TRUE, 
+#'               xlim = lonLim, ylim = latLim, xlab = slp$Dates$start[1])
+#'}
+
+
 getCyclonTrack <- function(cyclonCenter,
                            seek.radius = 10,
                            ndr.threshold = 1,
